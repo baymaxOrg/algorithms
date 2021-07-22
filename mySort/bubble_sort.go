c6d5d1c12fe1353cac4c900557fdeac812f5467c
unknown <sunpengfei@sc.local>
2021-07-22 11:39:00 +0800

    bubblesort 增加测试，添加注释。

--- mySort/bubble_sort.go
+++ mySort/bubble_sort.go
@@ -1,13 +1,18 @@
 package mysort
 
+// BubbleSort 冒泡排序
 func BubbleSort(nums []int) {
 	bubbleSort(nums)
 }
 
+// bubbleSort 冒泡排序的具体实现
 func bubbleSort(nums []int) {
+	// 控制便利的次数为slice的长度。
 	for i := 0; i < len(nums); i++ {
+		// 遍历无序区
 		for j := 0; j < len(nums)-i-1; j++ {
 			if nums[j] > nums[j+1] {
+				// 交换元素。大的换后边，
 				nums[j+1], nums[j] = nums[j], nums[j+1]
 			}
 		}
--- mySort/bubble_sort_test.go
+++ mySort/bubble_sort_test.go
@@ -2,13 +2,23 @@ package mysort_test
 
 import (
 	mysort "gitee.com/RocsSun/algorithms/mySort"
+	"gitee.com/RocsSun/algorithms/utils"
 	"testing"
 )
 
 func TestBubbleSort(t *testing.T) {
 	values := []int{4, 3, 2, 1, 0}
+	utils.ShuffleSlice(values)
 	mysort.BubbleSort(values)
 	if values[0] != 0 && values[1] != 1 && values[2] != 2 && values[3] != 3 && values[4] != 4 {
+		t.Error("bubble sort failed")
+	}
+}
+
+func TestBubbleSort2(t *testing.T) {
+	values := []int{4, 3, 2, 2, 0}
+	mysort.BubbleSort(values)
+	if values[0] != 0 && values[1] != 2 && values[2] != 2 && values[3] != 3 && values[4] != 4 {
 		t.Error("bubble sort failed")
 	}
 }
