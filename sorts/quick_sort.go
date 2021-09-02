04a6e809eb4964411c412b2470ec6bfcfc66b151
RocSun <710989028@qq.com>
2021-09-02 15:47:14 +0800

    快排完成，测试通过。

--- sorts/quick_sort.go
+++ sorts/quick_sort.go
@@ -1,51 +1,41 @@
 package sorts
 
-func QuickSort(nums []int) {
-	quickSort(nums, 0, len(nums)-1)
+// QuickSort 快排函数
+func QuickSort(nums []int) []int {
+	if len(nums) < 2 {
+		return nums
+	}
+	return quickSort(nums)
 }
 
-func quickSort(nums []int, left, right int) {
+// quickSort 预处理
+func quickSort(nums []int) []int {
+	_quickSort(nums, 0, len(nums)-1)
+	return nums
+}
+
+// _quickSort 快排
+func _quickSort(nums []int, left, right int) {
 	if left < right {
-		position := partition(nums, left, right)
-		quickSort(nums, left, position)
-		quickSort(nums, position+1, right)
+		middle := partition(nums, left, right)
+		_quickSort(nums, left, middle)
+		_quickSort(nums, middle+1, right)
 	}
 }
 
+// partition 移动元素。
 func partition(nums []int, left, right int) int {
 	tmp := nums[left]
-	for left < right {
-		for left < right && tmp <= nums[right] {
+	for right > left {
+		for right > left && tmp <= nums[right] {
 			right--
 		}
 		nums[left] = nums[right]
-
-		for left < right && tmp >= nums[left] {
+		for right > left && tmp >= nums[left] {
 			left++
 		}
 		nums[right] = nums[left]
 	}
 	nums[left] = tmp
 	return left
-}
-
-func _quickSort(nums []int, left, right int) {
-	i, j := left, right
-	if i < j {
-		tmp := nums[i]
-		for i < j {
-			for i < right && tmp <= nums[j] {
-				j--
-			}
-			nums[i] = nums[j]
-
-			for i < j && tmp >= nums[i] {
-				i++
-			}
-			nums[j] = nums[i]
-		}
-		nums[i] = tmp
-		_quickSort(nums, left, i)
-		_quickSort(nums, i+1, right)
-	}
 }
--- sorts/quick_sort_test.go
+++ sorts/quick_sort_test.go
@@ -3,12 +3,20 @@ package sorts_test
 import (
 	"testing"
 
-	mysort "gitee.com/RocsSun/algorithms/sorts"
+	ms "gitee.com/RocsSun/algorithms/sorts"
 )
+
+func TestQuickSort(t *testing.T) {
+	values := []int{4, 3, 2, 2, 0}
+	ms.QuickSort(values)
+	if values[0] != 0 && values[1] != 2 && values[2] != 2 && values[3] != 3 && values[4] != 4 {
+		t.Error("bubble sorts failed")
+	}
+}
 
 func TestQuickSort1(t *testing.T) {
 	values := []int{4, 3, 2, 1, 0}
-	mysort.QuickSort(values)
+	ms.QuickSort(values)
 	for i, v := range values {
 		if values[i] != v {
 			t.Error("quick sorts failed")
